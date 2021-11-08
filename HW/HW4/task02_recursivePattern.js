a17bb164ee3a36b465d61af41da21ddf77092e35
Kamagh <you@example.com>
2021-11-08 16:56:14 +0400

    HW4, task2 - abacabaPattern(effective solution)

--- HW/HW4/task02_recursivePattern.js
+++ HW/HW4/task02_recursivePattern.js
@@ -1,7 +1,8 @@
 function abacabaPattern(num) {
     //(a(n) = a(n - 1) + letter(n) + a(n - 1))
-    if (num <= 0) return '';                    //number in 36 form
-    return `${abacabaPattern(num - 1)}${(num + 9).toString(36)}${abacabaPattern(num - 1)}`
+    if (num <= 0) return '';     //number in 36 form
+    let recCall = abacabaPattern(num - 1)
+    return `${recCall}${(num + 9).toString(36)}${recCall}`
 }
 
-console.log(abacabaPattern(26));
+console.log(abacabaPattern(3));
