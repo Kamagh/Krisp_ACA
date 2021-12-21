670fbb172d9a24ede0e67d780e7a93a5bba9fbf8
Kamo Aghbalyan <kamo.aghbalyan@gmail.com>
2021-12-21 08:18:35 +0400

    Search bar

--- HW/HW6/script.js
+++ HW/HW6/script.js
@@ -1,17 +1,35 @@
 let countries;
 const countriesList = document.getElementById("countries");
 
+const countrySearch = document.getElementById("countrySearch");
+const searchButton = document.getElementById("searchButton")
+
 await fetch('https://restcountries.com/v3.1/all')
     .then(res => res.json())
     .then((data) => initialize(data))
     .catch(err => console.log('Error: ', err));
 
 countriesList.addEventListener("change", newCountrySelection/*event => /!*displayCountry(event.target.value)*!/console.log(event.target)*/);
+console.log(searchButton);
+searchButton.addEventListener("click", newCountrySearchBtn) //Searching with btn
+countrySearch.addEventListener("keyup", newCountrySearch) //searching with only writing
 
 function newCountrySelection(event) {
     const arg = event.target.value;
     //console.log(arg)
     displayCountry(arg);
+}
+
+function newCountrySearchBtn() {
+    const country = countries.find((country) => country.name.common === countrySearch.value);
+    displayCountry(country.name.common)
+}
+
+function newCountrySearch(event) {
+    const arg = event.target.value;
+    if (countrySearch.value === arg)
+        displayCountry(arg);
+    
 }
 
 function displayCountry(name) {
